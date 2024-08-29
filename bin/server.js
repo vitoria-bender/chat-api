6243b9cda5bc936b57b0d90b3aebbcbfd96dccf5
Vitoria-Daris-Bender <vitoria-bender1@educar.rs.gov.br>
2024-08-28 22:22:55 -0300

    meu deus do ceu

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const app = require("../src/api");
 
+app.use((req, res, next) => {
+    next();
+});
+
 let port = process.env.API_PORT || 3000;
 
 app.listen(port);
--- src/api.js
+++ src/api.js
@@ -20,15 +20,15 @@ app.use('/', router.get('/sobre', (req, res) =>{
     });
 }));
 
-app.use("/entrar", router.post("/entrar", async(req, res, next) =>{
-    const usuarioController = require ("./controllers/usuarioController");
+app.use("/entrar", router.post("/entrar", async(req, res) =>{
+    const usuarioController = require ("../src/controllers/usuarioController");
     let resp = await usuarioController.entrar(req.body.nick);
     res.status(200).send(resp);
 }));
 
-app.use("/salas", router.get("/salas", async(req, res, next)=>{
-    const token = require("../../util/token");
-    const salaController = require("./controllers/salaController");
+app.use("/salas", router.get("/salas", async(req, res)=>{
+    const token = require("../util/token");
+    const salaController = require("../src/controllers/salaController");
     const teste = await token.checkToken(req.headers.token, req.headers.iduser, req.headers.nick);
     if(teste){
         let resp = await salaController.get();
@@ -40,8 +40,8 @@ app.use("/salas", router.get("/salas", async(req, res, next)=>{
 }));
 
 app.use("/sala/entrar", router.post("/sala/entrar", async(req, res)=>{
-    const token = require("../../util/token");
-    const salaController = require("./controllers/salaController");
+    const token = require("../util/token");
+    const salaController = require("../src/controllers/salaController");
     if(token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)){
         let resp = await salaController.entrar(req.headers.iduser, req.query.idSala);
         res.status(200).send(resp);
@@ -51,16 +51,16 @@ app.use("/sala/entrar", router.post("/sala/entrar", async(req, res)=>{
 }));
 
 app.use("/sala/listar", router.get("/sala/listar", async (req, res) => {
-    const token = require("../../util/token");
-    const salaController = require("./controllers/salaController");
+    const token = require("../util/token");
+    const salaController = require("../src/controllers/salaController");
     if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
     let resp= await salaController.buscarMensagens(req.query.idSala, req.query.timestamp);
     res.status(200).send(resp);
 }));
 
 app.use("/sala/enviar", router.post("/sala/enviar", async (req, res) => {
-    const token = require("../../util/token");
-    const salaController = require("./controllers/salaController");
+    const token = require("../util/token");
+    const salaController = require("../src/controllers/salaController");
     if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
     let resp= await salaController.enviarMensagem(req.headers.nick, req.body.msg,req.body.idSala);
     res.status(200).send(resp);
